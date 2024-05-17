f4f82e6035f7eaeab99dea2e7d99dcb1af4ae2cb
SE-82\Batch 10 <lauwwulan1806@gmail.com>
2024-05-17 14:39:44 +0700

    Main commit day9

--- Day6/FuncDemo/obj/Debug/net8.0/FuncDemo.AssemblyInfo.cs
+++ Day6/FuncDemo/obj/Debug/net8.0/FuncDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FuncDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a50d865b52119a22b63acba040af30bdfad1ffec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3f1910914e5d07924c023014504b91be81d524")]
 [assembly: System.Reflection.AssemblyProductAttribute("FuncDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FuncDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
