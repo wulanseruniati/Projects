cadd6129543e42eb0f4b5cfd2467a9ac13bb4340
SE-82\Batch 10 <lauwwulan1806@gmail.com>
2024-05-17 14:39:10 +0700

    before merge

--- Day6/FuncDemo/obj/Debug/net8.0/FuncDemo.AssemblyInfo.cs
+++ Day6/FuncDemo/obj/Debug/net8.0/FuncDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FuncDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28a9e455d43f074b9368d10b1ed3902f378cd99d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a50d865b52119a22b63acba040af30bdfad1ffec")]
 [assembly: System.Reflection.AssemblyProductAttribute("FuncDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FuncDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
